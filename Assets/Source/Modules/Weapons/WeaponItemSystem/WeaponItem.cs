c5074cc644e0c63c58916a94d29cfbd38d1f06f1
sulphur34 <71759429+sulphur34@users.noreply.github.com>
2024-04-20 14:46:58 +0300

    Add WeaponTracker

---
+++ Assets/Source/Modules/Weapons/WeaponItemSystem/WeaponItem.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Source.Modules.Weapons.WeaponItemSystem
+{
+    public class WeaponItem : MonoBehaviour
+    {
+        public bool Equipped { get; private set; }
+    }
+}
