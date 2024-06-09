10fc7e752f2bee0736072fd4b629c3cf99711385
Claudiff <versandromede@gmail.com>
2024-06-10 02:03:01 +0300

    Added leaderboard display and authorization

---
+++ Assets/Source/Modules/LeaderboardSystem/LeaderboardUpdater.cs
@@ -0,0 +1,25 @@
+﻿using VContainer;
+
+namespace Modules.LeaderboardSystem
+{
+    public class LeaderboardUpdater
+    {
+        private Leaderboard _leaderboard;
+
+        [Inject]
+        public LeaderboardUpdater(Leaderboard leaderboard)
+        {
+            _leaderboard = leaderboard;
+        }
+
+        public void UpdateLeaderboard()
+        {
+            // _leaderboard.SetPlayer();
+        }
+
+        private void OnGameOver()
+        {
+
+        }
+    }
+}
